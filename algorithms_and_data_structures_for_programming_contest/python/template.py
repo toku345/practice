cd8665867933a8ef9eb698265f8ab700a7be3c94
Fumitaka Tokumitsu <toku345@gmail.com>
2019-10-18 12:35:01 +0900

    Add template.py

---
+++ algorithms_and_data_structures_for_programming_contest/python/template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import unittest
+
+### Test code
+
+class TestProblem(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+### Answer
+
+
+
+### entry point
+
+if __name__ == '__main__':
+    unittest.main()
