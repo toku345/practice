5e19d74fca43996fad523675bb6de8b60116fb45
Fumitaka Tokumitsu <toku345@gmail.com>
2020-01-10 06:40:29 +0900

    [螺旋本9.2] C++ 回答を Python3 に porting

---
+++ algorithms_and_data_structures_for_programming_contest/9.2/answer.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+
+class Node:
+    def __init__(self):
+        self.key: int = None
+        self.right: Node = None
+        self.left: Node = None
+        self.parent: Node = None
+
+
+def insert(k: int, root: Node) -> Node:
+    y = nil
+    x = root
+
+    z = Node()
+    z.key = k
+    z.left = nil
+    z.right = nil
+
+    while x != nil:
+        y = x
+        if z.key < x.key:
+            x = x.left
+        else:
+            x = x.right
+
+    z.parent = y
+    if y == nil:
+        root = z
+    else:
+        if z.key < y.key:
+            y.left = z
+        else:
+            if z.key < y.key:
+                y.left = z
+            else:
+                y.right = z
+
+    return root
+
+
+def inorder(u: Node) -> None:
+    if u == nil:
+        return
+    inorder(u.left)
+    print(f' {u.key}', end='')
+    inorder(u.right)
+
+
+def preorder(u: Node) -> None:
+    if u == nil:
+        return
+    print(f' {u.key}', end='')
+    preorder(u.left)
+    preorder(u.right)
+
+
+def main() -> None:
+    root = nil
+    n = int(input())
+
+    for _ in range(n):
+        inputs = input().split(' ')
+        if inputs[0] == 'insert':
+            root = insert(int(inputs[1]), root)
+        elif inputs[0] == 'print':
+            inorder(root)
+            print('')
+            preorder(root)
+            print('')
+
+
+nil = Node()
+
+if __name__ == '__main__':
+    main()
