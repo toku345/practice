fb910353fc7fdae7ddf027fa74be7ffe47a65ceb
Fumitaka Tokumitsu <toku345@gmail.com>
2020-01-16 13:18:49 +0900

    [螺旋本 10.2] Rustへポーティング

---
+++ algorithms_and_data_structures_for_programming_contest/chap10-2/src/main.rs
@@ -0,0 +1,46 @@
+use std::io;
+
+fn parent(i: usize) -> usize {
+    i / 2
+}
+
+fn left(i: usize) -> usize {
+    2 * i
+}
+
+fn right(i: usize) -> usize {
+    2 * i + 1
+}
+
+fn main() {
+    let h = read_line().unwrap().parse().unwrap();
+
+    let input: Vec<i32> = read_line()
+        .unwrap()
+        .split_whitespace()
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect();
+
+    let a = [vec![0], input].concat();
+
+    for i in 1..(h + 1) {
+        print!("node {}: key = {}, ", i, a[i]);
+        if parent(i) >= 1 {
+            print!("parent key = {}, ", a[parent(i)])
+        }
+        if left(i) <= h {
+            print!("left key = {}, ", a[left(i)]);
+        }
+        if right(i) <= h {
+            print!("right key = {}, ", a[right(i)]);
+        }
+        println!();
+    }
+}
+
+// Utilities
+fn read_line() -> io::Result<String> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    Ok(input.trim().to_string())
+}
