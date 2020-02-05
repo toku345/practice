e65e06ed86eef38d03744ea4f52e053f68115d3b
Fumitaka Tokumitsu <toku345@gmail.com>
2020-02-05 12:25:33 +0900

    [螺旋本 12.3] スタック版をPython3へポーティング

---
+++ algorithms_and_data_structures_for_programming_contest/chap12-3/answer_stack.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+from enum import Enum
+
+
+class DFS():
+    class Color(Enum):
+        WHITE = 0
+        GRAY = 1
+        BLACK = 2
+
+    def __init__(self, M, n):
+        self.M = M
+        self.n = n
+        self.color = [self.Color.WHITE for _ in range(self.n)]
+        self.d, self.f = [-1] * self.n, [-1] * self.n
+        self.tt = 0
+        self.nt = [0] * self.n
+
+    def next(self, u: int):
+        while self.nt[u] < self.n:
+            v = self.nt[u]
+            self.nt[u] = v + 1
+            if self.M[u][v] != 0:
+                return v
+        return -1
+
+    def dfs_visit(self, r: int):
+        S = []                  # list をスタックとして使う
+        S.append(r)
+        self.color[r] = self.Color.GRAY
+        self.tt += 1
+        self.d[r] = self.tt
+        while len(S) > 0:
+            # スタックの一番上（= listの末尾）の値を取得 ※ pop()するとlistからなくなるので[-1]でアクセス
+            u = S[-1]
+            v = self.next(u)
+            if v != -1:
+                if self.color[v] == self.Color.WHITE:
+                    self.color[v] = self.Color.GRAY
+                    self.tt += 1
+                    self.d[v] = self.tt
+                    S.append(v)
+            else:
+                S.pop()
+                self.color[u] = self.Color.BLACK
+                self.tt += 1
+                self.f[u] = self.tt
+
+    def dfs(self):
+        for u in range(self.n):
+            if self.color[u] == self.Color.WHITE:
+                self.dfs_visit(u)
+
+        for i in range(self.n):
+            print(f'{i+1} {self.d[i]} {self.f[i]}')
+
+
+def main():
+    n = int(input())
+    M = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        line = input()
+        inputs = [int(x) for x in line.split(' ')]
+        u = inputs.pop(0)
+        u -= 1                  # 0 オリジンへ変換
+        _ = inputs.pop(0)       # k （使わない...）
+        for v in inputs:
+            v -= 1              # 0 オリジンへ変換
+            M[u][v] = 1
+
+    DFS(M, n).dfs()
+
+
+if __name__ == '__main__':
+    main()
