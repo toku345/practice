5370f03df8ff8096b3cd77cd722088cc3694484c
Fumitaka Tokumitsu <toku345@gmail.com>
2020-07-08 08:42:15 +0900

    [TDD本: 多国籍通貨] Refactor

--- the_money_example_c++/main/money.cpp
+++ the_money_example_c++/main/money.cpp
@@ -2,9 +2,8 @@
 
 Money::Money(int amount) { this->amount = amount; }
 
-Money Money::times(int multiplier) {
-  return Money{ amount * multiplier };
-}
+Money Money::times(int multiplier) { return Money{amount * multiplier}; }
+
 
 bool Money::operator==(Money money) const {
   return amount == money.amount;
