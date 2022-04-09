3a7dbe1a790a462962442a33f32cb9f289e76338
Fumitaka Tokumitsu <fumitaka.tokumitsu@classi.jp>
2022-04-09 16:56:04 +0900

    :construction: 解答コードを置くため classlib プロジェクトを追加

---
+++ leetcode/csharp/Answers/Q1523.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Answers.Q1523;
+public class Solution
+{
+    public int CountOdds(int low, int high)
+    {
+        throw new NotImplementedException("not implemented!");
+    }
+}
