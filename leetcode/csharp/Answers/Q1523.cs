4f412da0b4f57996c34ab5b29a4377449c0de56b
Fumitaka Tokumitsu <fumitaka.tokumitsu@classi.jp>
2022-04-10 17:10:42 +0900

    :sparkles: 問題1523の解答を追加

--- leetcode/csharp/Answers/Q1523.cs
+++ leetcode/csharp/Answers/Q1523.cs
@@ -5,6 +5,14 @@ public class Solution
 {
     public int CountOdds(int low, int high)
     {
-        throw new NotImplementedException("not implemented!");
+        int count = 0;
+        for (int i = low; i <= high; i++)
+        {
+            if (i % 2 == 1)
+            {
+                count++;
+            }
+        }
+        return count;
     }
 }
